70e876f2662d71eaa2f901546ebe5f66a7202417
laiajudeztallon-blip <laiajudeztallon@gmail.com>
2025-12-05 12:19:44 +0100

    Update ej5a1.py

--- 5a/ej5a1.py
+++ 5a/ej5a1.py
@@ -59,7 +59,15 @@ Exemple:
 # Corret and overwrite class Student here 
 class Student:
     def __init__(self):
-        pass
+        self.name = name
+        self.age = age
+        self.average = average
+
+    def describe(self):
+        return f"Name: {self.name}, Age: {self.age}, Average: {self.average}"
+
+    def grade(self, new_grade):
+        self.average = (self.average + new_grade) / 2
        
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
